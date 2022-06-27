a9ed58a864a78a212ea964c7b11a8688aa553e44
khanhtruong92 <khanhtq@pascaliaasia.com>
2022-06-27 14:58:02 +0700

    add homework big o

---
+++ Algorithm/BigO/homework_1.php
@@ -0,0 +1,6 @@
+Which is the correct order of the following algorithms
+(selection, bubble, insertion, quick and merge sorts) 
+with respect to their time complexity in the best case?
+
+Answer
+Ω(n^2), Ω(n), Ω(n), Ω(n log(n)), Ω(n log(n))
---
+++ Algorithm/BigO/homework_2.php
@@ -0,0 +1,9 @@
+Which is the best algorithm following algorithms
+(selection, bubble, insertion, quick and merge sorts) 
+with respect to their time complexity in the worst case?
+
+Answer
+
+merge sort is the best algorithm with respect it time complexity in the worst case
+because it is O(n log(n))
+the rest are O(n^2)
