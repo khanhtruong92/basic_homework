76fc5182386248f1019b1f639c45b87374382850
khanhtruong92 <khanhtq@pascaliaasia.com>
2022-09-15 15:51:17 +0700

    add homework Section 7

---
+++ Js/Section7/homework-3.js
@@ -0,0 +1,43 @@
+let users = [
+    {
+        name: 'Larry',
+        phone: "213-555-1234",
+        address: "123 N 1st Ave"
+    },
+    {
+        name: 'Jane',
+        phone: "213-555-4321",
+        address: "123 N 1st Ave"
+    },
+    {
+        name: 'Sam',
+        phone: "213-555-5678",
+        address: "123 N 1st Ave"
+    },
+    {
+        name: 'Anne',
+        phone: "213-555-9876",
+        address: "123 N 1st Ave"
+    },
+    {
+        name: 'David',
+        phone: "213-555-0912",
+        address: "123 N 1st Ave"
+    }
+];
+
+// Add new user follow data
+
+users.push({
+    name: 'Wayne',
+    phone: "213-112-0912",
+    address: "123 N 1st Ave"
+});
+
+const Users = new Map();
+// Đưa các phần tử mảng vào Map()
+for (let i = 0; i < users.length; i++) {
+    Users.set(users[i].name, users[i]);
+    delete (Users.get(users[i].name)).name;
+};
+console.log(Users.get('Wayne'));
