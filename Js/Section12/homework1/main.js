55641a46af19ee65ffb4831e608791b212636310
khanhtruong92 <khanhtq@pascaliaasia.com>
2022-09-21 17:06:48 +0700

    add comments to the file

--- Js/Section12/homework1/main.js
+++ Js/Section12/homework1/main.js
@@ -2,10 +2,12 @@ const button = document.querySelector("button");
 const pE1 = document.getElementById("color");
 const bgE1 = document.getElementById("container");
 
+// Tạo một mảng chứa các giá trị màu để chọn ngẫu nhiên
 const color = ['Red', 'Green', 'Blue', 'Yellow', 'Pink', 'Purple', 'Gray', 'Chocolate', 'Coral', 'Cyan'];
 
 button.addEventListener('click', changeBackground);
 
+// Tạo hàm trả về giá trị màu 
 function changeBackground() {
     const colorsIndex = Math.floor(Math.random()*color.length);
     button.style.backgroundColor = color[colorsIndex];
